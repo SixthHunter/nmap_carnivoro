de922c3333b657a90d3ee5e8219fd25225c81445
Sixth Hunter <6sixth6hunter6@gmail.com>
2020-07-25 23:02:53 -0300

    Adicionando umm novo codigo para portscan.

--- nmap_carnivoro.py
+++ nmap_carnivoro.py
@@ -3,5 +3,17 @@
 # 23/07/2020
 print("NMAP CARNIVORO")
 print("Sixth Hunter")
-print("Anonymous")
-print("[1] - CONECTAR AO BACKDOOR]")
+print("[1] - PORT SCANNER")
+
+import socket
+import sys
+
+portas = [80, 443, 21, 22, 23, 25, 3306]
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+for porta in portas:
+    if sock.connect_ex((sys.argv[1], porta)):
+        print("Porta ", porta, "aberta")
+
+print("CONCLUÍDO")
+sock.close
